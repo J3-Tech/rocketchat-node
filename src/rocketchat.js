2c6ae7718e3f74e13b95204b9b6614aa6a9b4aae
Dickriven Chellemboyee <jchellem@gmail.com>
2017-03-07 16:21:01 +0400

    cleanup

--- src/rocketchat.js
+++
@@ -1,8 +0,0 @@
-import Authentication from './authentication'
-import Chat from './chat'
-
-RocketChat = {};
-RocketChat.Authentication = Authentication;
-RocketChat.Chat = Chat;
-
-module,exports = RocketChat;
